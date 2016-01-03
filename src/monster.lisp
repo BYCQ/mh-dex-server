f8c3be55617f896bd84a43b0dd9fed11707cca34
Break Yang <breakds@samaritan.breakds.org>
2016-01-03 17:14:09 -0500

    monster harvest item added

--- src/monster.lisp
+++ src/monster.lisp
@@ -5,6 +5,7 @@
     (:import-from :mh-dex.common
                   :with-dex-queries
                   :make-monster-key
+                  :make-item-key
                   :lang-text)
     (:export :*monsters*
              :reload-monsters
@@ -24,16 +25,35 @@
                                         "Mon_Name_0" "Mon_Name_1" "Mon_Name_3")
                                (:from "ID_Mon_Name")
                                (:where "Mon_ID > -1")
-                               (:order-by "Mon_ID")))
-    (loop
-       for id from 0
-       for (dex-id en zh jp) in monsters
-       do (let ((key (make-monster-key id)))
-            (assert (= dex-id id))
-            (push (list :id id
-                        :key key
-                        :name (lang-text :en en :zh zh :jp jp))
-                  *monsters*))))
+                               (:order-by "Mon_ID"))
+                     (items (:select "Mon_ID" "Itm_ID" "Qty"
+                                     "Pct" "Rank_ID"
+                                     ;; names: 0 = en, 1 = zh, 3 = jp
+                                     "Mon_GetItmType_0"
+                                     "Mon_GetItmType_1"
+                                     "Mon_GetItmType_3")
+                            (:from "DB_ItmtoMon")
+                            (:inner-join "ID_Mon_GetItmType"
+                                         "DB_ItmtoMon.Mon_GetItmType_ID = Id_Mon_GetItmType.Mon_GetItmType_ID")))
+    (let ((items-table (make-hash-table)))
+      (loop for (dex-id dex-item-id quantity probability rank en zh jp)
+         in items
+         do (push (list :key (make-item-key (1- dex-item-id))
+                        :probability (round (* probability 100))
+                        :rank rank
+                        :approach (lang-text :en en :zh zh :jp jp))
+                  (gethash dex-id items-table nil)))
+      
+      (loop
+         for id from 0
+         for (dex-id en zh jp) in monsters
+         do (let ((key (make-monster-key id)))
+              (assert (= dex-id id))
+              (push (list :id id
+                          :key key
+                          :items (gethash dex-id items-table)
+                          :name (lang-text :en en :zh zh :jp jp))
+                    *monsters*)))))
   
   (setf *monsters* (nreverse *monsters*))
 
